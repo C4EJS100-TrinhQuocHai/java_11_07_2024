1624b5ca7ad00ef42bf74b6c29fb7d972df17c95
trinhquochai <trinhquochai12@gmail.com>
2024-07-31 07:50:17 +0700

    session08

---
+++ session08-jsbasic/lythuyet/index.js
@@ -0,0 +1,30 @@
+document.write("rikkei academy!");
+/* 
+    var, let, const
+    - biết cách khai báo
+    -3 ông này khác nhau như thế nào?
+*/
+// khai báo tuổi của 1 người
+// var age = 18;
+// // age=20;
+// var age=20;
+// console.log("11111112222:",age);
+const  age=20;
+// age=30;
+console.log(11111,age);
+let fullName ='Vũ Hồng Vân';
+var firstName;
+console.log("firstName", firstName);
+let checkEmailLogin;
+let $ =7;
+let a=100;
+let A=100;
+let _ =8;
+// let ! =77;
+let class1="jv";
+let isBoolean= 5>4;
+console.log(isBoolean);
+console.log("giá trị tên của bạn là: ",fullName);
+let checkIsNull= null;
+let checkIsNaN= "JS"-5;
+console.log(222222444,checkIsNaN);
